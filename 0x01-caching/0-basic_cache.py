6d5a90b95bc2b7820a7bde2ead46da0a99b9c030
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-24 01:27:55 -0700

    Task 0 (DONE) ✔

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+"""cache system"""
 
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """a class BasicCache that inherits from BaseCaching"""
+    def __init__(self):
+        """init method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """add an item to the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """get an item from the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
