ff05647d62c06b6a5af1d63dead0e37182d2363e
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-24 04:04:46 -0700

    Task 3 (DONE) ✔

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,2 +1,36 @@
 #!/usr/bin/python3
+"""cache system"""
 
+from collections import OrderedDict
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """a class LRUCache that inherits from BaseCaching"""
+    def __init__(self):
+        """init method"""
+        super().__init__()
+        self.capacity = BaseCaching.MAX_ITEMS
+        self.order = OrderedDict()
+
+    def put(self, key, item):
+        """add an item to the cache"""
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+        self.order[key] = item
+        self.order.move_to_end(key)
+
+        if len(self.cache_data) > self.capacity:
+            key, item = self.order.popitem(last=False)
+            del self.cache_data[key]
+            print("DISCARD: {}".format(key))
+
+    def get(self, key):
+        """get an item from the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        self.order.move_to_end(key)
+        return self.cache_data.get(key)
