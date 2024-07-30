e554878e3dda0bbc818f044b92d8b055a761adcb
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-30 09:01:31 -0700

    Task 5 ✔

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""Flask app"""
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config:
+    """Config Class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config.from_object(Config)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@app.route('/')
+def home():
+    """home function"""
+    return render_template('5-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale function"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+def get_user():
+    """Get user function"""
+    user_id = request.args.get('user_id')
+    if user_id:
+        return users.get(int(user_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """Before request function"""
+    g.user = get_user()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
